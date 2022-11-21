fd66971c4388aa29af78e37098643ce0fd749307
Ace_Radom <sichenradomlyu@gmail.com>
2022-11-21 15:54:38 +0100

    add shell-start bat

---
+++ start_shell.bat
@@ -0,0 +1,8 @@
+@echo off
+
+@rem this bat is for running PCSX2_Memcard_Manager_Shell without accessing ./bin
+@rem full with dll isn't good for normal user...
+
+.\bin\p2mm_shell
+
+@rem run PCSX2_Memcard_Manager_Shell
